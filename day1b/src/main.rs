c1235ed15087bd8b9fd93ceb5c6fdfbf25bebecd
Linus Kardell <linus.kardell@gmail.com>
2020-12-05 23:19:57 +0100

    Add day 1

---
+++ day1b/src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    println!("{}", input.chars().enumerate().try_fold(0, |acc, (n, x)| match x {
+        '(' => Ok(acc + 1),
+        ')' => if acc > 0 { Ok(acc - 1) } else { Err(n + 1) },
+        _ => Ok(acc),
+    }).unwrap_err());
+}
