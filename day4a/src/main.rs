b039e30f82ec9e8e4f7f7336af2d9ea2737beb6e
Linus Kardell <linus.kardell@gmail.com>
2020-12-13 20:42:41 +0100

    Add day 4

---
+++ day4a/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut input = Vec::new();
+    io::stdin().read_to_end(&mut input).unwrap();
+    let orig_len = input.len();
+    println!("{}", (0..).find(|&x| {
+        input.truncate(orig_len);
+        input.append(&mut format!("{}", x).into_bytes());
+        let hash = md5::compute(&input);
+        (0..2).all(|y| hash.0[y] == 0) && hash.0[2] < 0x10
+    }).unwrap());
+}
