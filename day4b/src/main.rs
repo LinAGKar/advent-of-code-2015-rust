578393fb0109d7e7220fb342e30a2c2c8dae52c3
Linus Kardell <linus.kardell@gmail.com>
2020-12-21 23:26:49 +0100

    Multithreading for day 4
    Though it doesn't actuall scale that well at this short runtime

--- day4b/src/main.rs
+++ day4b/src/main.rs
@@ -1,14 +1,49 @@
-use std::io;
 use std::io::Read;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let mut input = Vec::new();
-    io::stdin().read_to_end(&mut input).unwrap();
+    std::io::stdin().read_to_end(&mut input).unwrap();
     let orig_len = input.len();
-    println!("{}", (0..).find(|&x| {
-        input.truncate(orig_len);
-        input.append(&mut format!("{}", x).into_bytes());
-        let hash = md5::compute(&input);
-        (0..3).all(|y| hash.0[y] == 0)
-    }).unwrap());
+
+    let answer_mutex = Arc::new(Mutex::new(None));
+
+    let cores = num_cpus::get();
+
+    let threads: Vec<_> = (0..cores).map(|i| {
+        let mut data = input.clone();
+        let answer_mutex = Arc::clone(&answer_mutex);
+
+        std::thread::spawn(move || {
+            if let Some(Some(answer)) = (0..).map(|x| x * cores + i).find_map(|x| {
+                if x % (100 * cores + i) == 0 {
+                    if let Some(found_answer) = *answer_mutex.lock().unwrap() {
+                        if x > found_answer {
+                            return Some(None)
+                        }
+                    }
+                }
+
+                data.append(&mut format!("{}", x).into_bytes());
+                let hash = md5::compute(&data);
+                if (0..3).all(|y| hash.0[y] == 0) {
+                    Some(Some(x))
+                } else {
+                    data.truncate(orig_len);
+                    None
+                }
+            }) {
+                let mut found_answer = answer_mutex.lock().unwrap();
+                if (*found_answer).unwrap_or(usize::MAX) > answer {
+                    *found_answer = Some(answer);
+                }
+            }
+        })
+    }).collect();
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    println!("{}", answer_mutex.lock().unwrap().unwrap());
 }
